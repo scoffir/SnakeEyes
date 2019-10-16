8b821a008e50f4765888da6831963060ff3cdb03
Karine Samyn <Karine.Samyn@hogent.be>
2019-10-16 16:10:59 +0200

    Add classes Dice and SnakeEyes (Play method not implemented)

---
+++ SnakeEyesGame/Models/SnakeEyes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SnakeEyesGame.Models
+{
+
+    public class SnakeEyes
+    {
+        #region Fields
+        private readonly Dice _eye1;
+        private readonly Dice _eye2;
+        #endregion
+
+        #region Properties
+
+        public int Total { get; private set; }
+
+        public int Eye1 => _eye1.Pips;
+
+        public int Eye2 => _eye2.Pips;
+        #endregion
+
+        #region Constructor
+        public SnakeEyes()
+        {
+            _eye1 = new Dice();
+            _eye2 = new Dice();
+            Total = 0;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Rolls the dices. Calculates the total.
+        /// </summary>
+        public void Play()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
